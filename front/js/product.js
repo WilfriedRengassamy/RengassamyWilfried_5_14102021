fb5b5233df0b39607da6c0d95676c0b58c9e1da0
Wilfried Rengassamy <wi.rengassamy@gmail.com>
2021-11-19 01:37:34 +0100

    page produit import produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,27 +1,51 @@
-const itemId = window.location.search;
-console.log(itemId);
-const searchParams = new URLSearchParams(itemId);
+const itemUrlId = window.location.search;
+console.log(itemUrlId);
+const searchParams = new URLSearchParams(itemUrlId);
 
-// Itère sur les paramètres de recherche
-for (let product of searchParams) {
-  console.log(product);
-}
+let itemId = itemUrlId.substr(4);
+let item;
 
 //Requête de l'API
-
-async function getItem() {
-  await fetch("http://localhost:3000/api/products")
+const getItem = async () => {
+  await fetch("http://localhost:3000/api/products/" + itemId)
     .then((res) => res.json())
-    .then((data) => (product = data));
-  //console.log(items);
-}
+    .then((data) => (item = data));
+};
 
-//Test affichage éléments vers product.html
-function showItemImg() {
-  let itemPic = document.getElementsByClassName("item__img");
-  itemPic[0].src = "../../back/images/kanap01.jpeg";
-  //let item = document.getElementsByClassName("item");
-  console.log(itemPic);
-  itemPic.append(itemPic);
-}
-showItemImg();
+//Affiche le produit
+const showItem = async () => {
+  await getItem();
+
+  //Met le nom du produit en titre de page
+  document.title = item.name;
+
+  //Affiche le nom du produit
+  let itemName = document.getElementById("title");
+  itemName.append(item.name);
+
+  //Affiche l'image du produit
+  let itemImg = document.getElementsByClassName("item__img");
+  console.log(itemImg[0]);
+  let img = document.createElement("img");
+  img.setAttribute("src", item.imageUrl);
+  itemImg[0].append(img);
+
+  //Affiche le prix du produit
+  let itemPrice = document.getElementById("price");
+  itemPrice.append(item.price);
+
+  //Affiche la description du produit
+  let itemDescription = document.getElementById("description");
+  itemDescription.append(item.description);
+
+  //définit les couleurs sélectionnable pour le produit
+  let colorsList = document.getElementById("colors");
+  let colors = item.colors;
+  for (let i = 0; i < colors.length; i++) {
+    let option = document.createElement("option");
+    option.value = colors[i];
+    option.text = colors[i];
+    colorsList.appendChild(option);
+  }
+};
+showItem();
