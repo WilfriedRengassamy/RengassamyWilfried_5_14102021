3b2f2d8ef65ef0affd5ceeb3d684bcc312155148
Wilfried Rengassamy <wi.rengassamy@gmail.com>
2021-12-05 15:21:14 +0100

    affichage des item du cart, problème cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,5 @@
 let cartItems = document.getElementById("cart__items");
-let item;
+//let item;
 
 //récupérer le localStorage sous forme d'array
 let cartLinea = localStorage["cart"];
@@ -38,16 +38,60 @@ let cart = JSON.parse(cartLinea);
 let totalQuantity = document.getElementById("totalQuantity");
 let cartPrice = document.getElementById("totalPrice");
 */
-for (let i of cart) {
+for (let item of cart) {
+  let cartItems = document.getElementById("cart__items");
   let cartItem = document.createElement("article");
-  cartItem.setAttribute("class", "cart__item");
+  cartItem.classList.add("cart__item");
   cartItem.setAttribute("data-id", item.id);
   let cartItemImgDiv = document.createElement("div");
+  cartItemImgDiv.classList.add("cart__item__img");
   let cartItemImg = document.createElement("img");
   cartItemImg.setAttribute("src", item.imageUrl);
   cartItemImg.setAttribute("alt", item.altTxt);
-  cartItemImgDiv.appendChild(cartItemImg);
+  let cartItemContent = document.createElement("div");
+  cartItemContent.classList.add("cart__item__content");
+  let cartItemContentTitlePrice = document.createElement("div");
+  cartItemContentTitlePrice.classList.add("cart__content__titlePrice");
+  let cartItemContentItemName = document.createElement("h2");
+  let cartItemContenItemPrice = document.createElement("p");
+  let cartItemContentSettings = document.createElement("div");
+  cartItemContentSettings.classList.add("cart__item__content__settings");
+  let cartItemContentSettingsQuantity = document.createElement("div");
+  cartItemContentSettingsQuantity.classList.add(
+    "cart__item__content__settings__quantity"
+  );
+  let cartItemContentSettingsQuantityName = document.createElement("p");
+  let cartItemContentSettingsQuantityInput = document.createElement("input");
+  cartItemContentSettingsQuantityInput.classList.add("itemQuantity");
+  cartItemContentSettingsQuantityInput.setAttribute("type", "number");
+  let cartItemContentSettingsDelete = document.createElement("div");
+  cartItemContentSettingsDelete.classList.add(
+    "cart__item__content__settings__delete"
+  );
+  let cartItemContentSettingsDeleteItem = document.createElement("p");
+  cartItemContentSettingsDeleteItem.classList.add("deleteItem");
+  cartItems.appendChild(cartItem);
   cartItem.appendChild(cartItemImgDiv);
+  cartItem.appendChild(cartItemContent);
+  cartItemContent.appendChild(cartItemContentTitlePrice);
+  cartItemContentTitlePrice.appendChild(cartItemContentItemName);
+  cartItemContentTitlePrice.appendChild(cartItemContenItemPrice);
+  cartItemContentItemName.append(item.name);
+  cartItemContenItemPrice.append(item.price);
+  cartItemImgDiv.appendChild(cartItemImg);
+  cartItemContent.appendChild(cartItemContentSettings);
+  cartItemContentSettings.appendChild(cartItemContentSettingsQuantity);
+  cartItemContentSettingsQuantity.appendChild(
+    cartItemContentSettingsQuantityName
+  );
+  cartItemContentSettingsQuantityName.append("Qté :");
+  cartItemContentSettingsQuantity.appendChild(
+    cartItemContentSettingsQuantityInput
+  );
+  cartItemContentSettingsQuantityInput.append(item.quantity);
+  cartItemContentSettings.appendChild(cartItemContentSettingsDelete);
+  cartItemContentSettingsDelete.appendChild(cartItemContentSettingsDeleteItem);
+  cartItemContentSettingsDeleteItem.append("Supprimer");
 }
 
 //----------------------------------------------- Travailler sur le formulaire -------------------------------------------------------
